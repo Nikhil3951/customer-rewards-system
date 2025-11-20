4db8d3fe9bf232cbada1c8e1dd8a09be6d8a47db
Nikhil3951 <75799032+Nikhil3951@users.noreply.github.com>
2025-11-21 01:09:53 +0530

    Update TransactionsDTO.java

--- src/main/java/com/retail/rewards/dto/TransactionsDTO.java
+++ src/main/java/com/retail/rewards/dto/TransactionsDTO.java
@@ -3,18 +3,19 @@ package com.retail.rewards.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 public class TransactionsDTO {
 
-    Double amount;
+    BigDecimal amount;
     String transactionId;
     LocalDate transactionDate;
     int pointsEarned;
 
-    public TransactionsDTO(Double amount, String transactionId, LocalDate transactionDate, int pointsEarned) {
+    public TransactionsDTO(BigDecimal amount, String transactionId, LocalDate transactionDate, int pointsEarned) {
         this.amount = amount;
         this.transactionId = transactionId;
         this.transactionDate = transactionDate;
